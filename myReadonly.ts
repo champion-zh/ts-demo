dc85be0c9fe16f93171cf5e27e9e55a6fb5e8e6f
张学文 <xuewen.zhang@yintech.cn>
2022-07-28 14:01:48 +0800

    添加注释

--- myReadonly.ts
+++ myReadonly.ts
@@ -2,7 +2,7 @@ interface Todo {
   title: string,
   description: string
 }
-
+// 该 Readonly 会接收一个 泛型参数，并返回一个完全一样的类型，只是所有属性都会被 readonly 所修饰。
 type MyReadonly<T> = {
   readonly [p in keyof T]: T[p]
 }
